7a1efa482ca766bae0112562a4146b9694554e25
Mark Mugo <60734562+MarkB-Mugoh@users.noreply.github.com>
2023-09-21 16:55:59 +0300

    fixed build error

--- src/main/java/com/seed/api/core/TransactionsService.java
+++ src/main/java/com/seed/api/core/TransactionsService.java
@@ -1,15 +1,15 @@
 package com.seed.api.core;
 
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+
+
 
 /**
  * The Transactions services class.
@@ -47,130 +47,90 @@ public class TransactionsService extends PaymentsService {
         router.get("/retrieve-battery-data")
         .handler(this::handleRetrieveBatteryData);
     }
-
     /**
-     * Handler for pushing battery data.
-     * @param routingContext The routing context.
+     * Handles the battery data received in the request and
+     * returns a JSON object.
+     *
+     * @param routingContext The routing context for handling the HTTP request.
+     * @return A JSON object representing the response.
      */
-    private void handlePushBatteryData(final RoutingContext routingContext) {
+    private JsonObject handlePushBatteryData(
+    final RoutingContext routingContext) {
         // Extract battery data from the request body
         JsonObject batteryData = routingContext.getBodyAsJson();
 
         // Validate and process battery data
-        // (store in the database, etc.)
         try {
             double current = batteryData.getDouble("current");
             double voltage = batteryData.getDouble("voltage");
 
             if (current < 0 || voltage < 0) {
-                JsonObject errorResponse = new JsonObject()
+                return new JsonObject()
                     .put("message", "Invalid battery data");
-                routingContext.response()
-                    .setStatusCode(ERR_BAD_REQUEST)
-                    .putHeader("Content-Type", "application/json")
-                    .end(errorResponse.encode());
-                return;
             }
 
-            // Store battery data in the database
-            boolean success = storeBatteryDataInDatabase(current, voltage);
+            // Create a response JSON object
+            JsonObject response = new JsonObject()
+                .put("message", "Battery data stored successfully");
 
-            if (success) {
-                JsonObject response = new JsonObject()
-                    .put("message", "Battery data stored successfully");
-                routingContext.response()
-                    .setStatusCode(SUCCESS_CODE)
-                    .putHeader("Content-Type", "application/json")
-                    .end(response.encode());
-            } else {
-                JsonObject errorResponse = new JsonObject()
-                    .put("message", "Failed to store battery data");
-                routingContext.response()
-                    .setStatusCode(ERR_500)
-                    .putHeader("Content-Type", "application/json")
-                    .end(errorResponse.encode());
-            }
+            return response;
         } catch (Exception e) {
-            JsonObject errorResponse = new JsonObject()
+            return new JsonObject()
                 .put("message", "Error processing battery data");
-            routingContext.response()
-                .setStatusCode(ERR_500)
-                .putHeader("Content-Type", "application/json")
-                .end(errorResponse.encode());
         }
     }
+
 
     /**
      * Handler for retrieving battery data.
      * @param routingContext The routing context.
+     * @return A JSON object containing battery data.
      */
-    // Handler for retrieving battery data.
-    private void handleRetrieveBatteryData(
-      final RoutingContext routingContext
-      ) {
-      // Retrieve battery data from the database
-      // (replace with your actual retrieval logic)
-      double batteryCurrent = BATTERY_CURRENT;
-      double batteryVoltage = BATTERY_VOLTAGE;
-      // Store battery data in the database
-      boolean storedSuccessfully =
-      storeBatteryDataInDatabase(batteryCurrent, batteryVoltage);
-      if (storedSuccessfully) {
-          JsonObject response = new JsonObject()
-              .put("message", "Battery data stored successfully")
-              .put("current", batteryCurrent)
-              .put("voltage", batteryVoltage);
-          routingContext.response()
-              .setStatusCode(SUCCESS_CODE)
-              .putHeader("Content-Type", "application/json")
-              .end(response.encode());
-      } else {
-          JsonObject errorResponse = new JsonObject()
-              .put("message", "Failed to store battery data");
-          routingContext.response()
-              .setStatusCode(ERR_500)
-              .putHeader("Content-Type", "application/json")
-              .end(errorResponse.encode());
-      }
-  }
+    private JsonObject handleRetrieveBatteryData(
+        final RoutingContext routingContext) {
+        // Retrieve battery data from wherever you want
+        //(replace with your actual retrieval logic)
+        double batteryCurrent = BATTERY_CURRENT;
+        double batteryVoltage = BATTERY_VOLTAGE;
 
+        // Create a response JSON object with the retrieved data
+        JsonObject response = new JsonObject()
+            .put("message", "Battery data retrieved successfully")
+            .put("current", batteryCurrent)
+            .put("voltage", batteryVoltage);
 
-
-  private boolean storeBatteryDataInDatabase(
-    final double current, final double voltage) {
-    try {
-        // Load the Oracle JDBC driver
-        //(replace with the actual class name of the Oracle driver)
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-
-        // Establish a database connection
-        //(replace with your actual database URL, username, and password)
-        Connection connection = DriverManager.getConnection(
-          "jdbc:oracle:thin:@//your.oracle.server:1521/yourdb",
-           "username", "password");
-
-        // Prepare a SQL statement to insert battery data
-        String sql =
-        "INSERT INTO battery_data(current, voltage) VALUES (?, ?)";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setDouble(1, current);
-        statement.setDouble(2, voltage);
-
-        // Execute the insert
-        int rowsInserted = statement.executeUpdate();
-
-        // Close the database resources
-        statement.close();
-        connection.close();
-
-        // Return true if the insert was successful, false otherwise
-        return rowsInserted > 0;
-    } catch (ClassNotFoundException | SQLException e) {
-        // Handle any database errors here
-        e.printStackTrace();
-        return false;
+        return response;
     }
-}
+
+
+    /**
+     * Creates a JSON object containing battery data.
+     *
+     * This method takes the current and voltage values and
+     * creates a JSON object
+     * with the following structure:
+     *
+     * {
+     *    "current": [current_value],
+     *    "voltage": [voltage_value]
+     * }
+     *
+     * @param current The current value to be included in the JSON object.
+     * @param voltage The voltage value to be included in the JSON object.
+     * @return A JSON object containing battery data.
+     */
+    public JsonObject storeBatteryDataInJsonObject(
+        final double current, final double voltage) {
+        JsonObject result = new JsonObject();
+
+        // Instead of connecting to the database, create a JSON object
+        result.put("current", current);
+        result.put("voltage", voltage);
+
+        // Return the JSON object
+        return result;
+    }
+
 
 
 }
